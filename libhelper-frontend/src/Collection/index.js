a7a65f883d5681c3f8fdfc413b74b6a5b7035783
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-06-25 10:19:37 -0700

    edit book card

--- libhelper-frontend/src/Collection/index.js
+++ libhelper-frontend/src/Collection/index.js
@@ -13,7 +13,7 @@ const sampleCollection = {
     {id: 2, title: "Test Book", img: "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg"},
     {id: 3, title: "Test Book", img: "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg"},
     {id: 4, title: "Test Book", img: "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg"},
-    {id: 5, title: "Test Book", img: "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg"},
+    {id: 5, title: "Test Book With a Really Long Title", img: "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg"},
   ]}
 
 function Collection() {
@@ -33,7 +33,7 @@ function Collection() {
     <Container>
       <h3>{collection.name}</h3>
       <p>{collection.description}</p>
-      <Row>
+      <Row className="justify-content-center">
         {collection.books.map(book => <BookCard key={book.id} book={book} />)}
       </Row>
     </Container>
--- libhelper-frontend/src/shared/BookCard/index.js
+++ libhelper-frontend/src/shared/BookCard/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {Card} from 'react-bootstrap'
 
 function BookCard({book}) {
   const history = useHistory();
 
-  handleClick(id) {
+  function handleClick(id) {
     history.push(`/book/${id}`);
   }
 
-
+  return (
+    <Card className="book mx-2 my-2 col-3 px-0" onClick={() => handleClick(book.id)}>
+      <Card.Img variant="top" src={book.img} />
+      <Card.Body className="my-0">
+        <Card.Title className="my-0">{book.title}</Card.Title>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default BookCard;
